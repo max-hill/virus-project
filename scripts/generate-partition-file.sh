cdb3837ebe4061ca7609c97d735caa321ece6e73
max-hill <70863495+max-hill@users.noreply.github.com>
2022-03-12 18:16:36 -0600

    completed commit

--- scripts/generate-partition-file.sh
+++ scripts/generate-partition-file.sh
@@ -23,7 +23,7 @@
 # (http://www.sbcl.org/)
 #
 # EXAMPLE USAGE:
-# 'bash generate-partition-file.sh 144551 500 >../data/partition-144551-500.txt'
+# 'bash generate-partition-file.sh 144551 500 >../data/partition-A144551-B500.txt'
 
 
 ### ---Begin script---
@@ -34,22 +34,13 @@
 A="$1"
 B="$2"
 
-# Call the lisp function
+# Call the lisp functions
 sbcl --noinform --eval '
-(progn
-  (defparameter *A* (parse-integer (second *posix-argv*)))
-  (defparameter *B* (parse-integer (third *posix-argv*)))
-  (defun generate-partition-file (alignment-length block-length &optional (block-starting-position 1) (gene-number 1))
-    "INPUT: two positive integers: (1) total alignment length and (2) desired
-block length. OUTPUT: the contents of an msa partition file. USAGE: 
-(make-partition-file 14125 500). The optional variables are internal (dummy) 
-variables and are not to be specified by the user. 2022-03-07 max-hill"
-    (if (> (+ block-starting-position block-length) alignment-length)
-	(format t "DNA, gene_~a ~a-~a~%" gene-number block-starting-position alignment-length)
-      (progn (format t "DNA, gene_~a ~a-~a~%" gene-number block-starting-position (+ block-starting-position block-length -1))
-	     (generate-partition-file alignment-length block-length (+ block-starting-position block-length) (1+ gene-number)))))
-  (generate-partition-file *A* *B*)
-  (quit))' $A $B 2>/dev/null
+(progn 
+       (load "generate-partition-file-aux.lisp")
+       (generate-partition-file *A* *B*)
+       (quit)) 
+' $A $B 2>/dev/null
 
 # Note that SBCL ('steel bank common lisp') is an implmentation of common lisp.
 # When loading files, SBCL outputs style warnings by default. Since this might
@@ -59,3 +50,6 @@ variables and are not to be specified by the user. 2022-03-07 max-hill"
 ################################################################################
 
 ### ---Script ends here---
+###~*position-padding*,'0d
+
+
