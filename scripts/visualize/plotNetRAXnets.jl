4a8b1569e24f203ece03262c9bde969698924115
Cecile Ane <cecileane@users.noreply.github.com>
2025-05-29 15:33:57 -0500

    new plot of NetRAX nets, 15g, 8tax

--- scripts/visualize/plotNetRAXnets.jl
+++ scripts/visualize/plotNetRAXnets.jl
@@ -66,36 +66,40 @@ R"dev.off()"
 l2r_str = readline("../analysis/netrax/multistart-titanium/multistart-titanium_inferred_network.nw",keep=false);
 l2r_infnet = replace(l2r_str,r"#(?<hybrid>[\d]+):" => s"#H\g<hybrid>:") |>
     (x -> replace(x,r"NODE_[\d.]+" => s"")) |> readTopology; # l2r_infnet.numHybrids: 6
-l2r_infnet_no3cyc = deepcopy(l2r_infnet);
-shrink3cycles!(l2r_infnet_no3cyc); # l2r_infnet_no3cyc.numHybrids: 2
-
 findfirst("Titanium_IBR_MLV_vaccine" .== map(x -> x.name,l2r_infnet.node)) # 25
 l2r_infnet.node[25].name = "Ti"
 
-findfirst("Titanium_IBR_MLV_vaccine" .== map(x -> x.name,l2r_infnet_no3cyc.node)) # 17
-l2r_infnet_no3cyc.node[17].name = "Ti";
-
-R"pdf(file='../figures/netrax/multistart-set1b-L2R-15genes-no3cyc.pdf',width=8,height=8)"
+## Multistart (15 genetrees), set1b, L2R, 15 genes, best network, 3-cycles not removed
+l2r_infnet_no3cyc = deepcopy(l2r_infnet);
+shrink3cycles!(l2r_infnet_no3cyc); # l2r_infnet_no3cyc.numHybrids: 2
 rootonedge!(l2r_infnet_no3cyc,13);
-rotate!(l2r_infnet_no3cyc,-12);
-rotate!(l2r_infnet_no3cyc,-4);
-rotate!(l2r_infnet_no3cyc,-8);
-rotate!(l2r_infnet_no3cyc,-17);
-rotate!(l2r_infnet_no3cyc,-3);
+for i in [-13,-4,-8,-17,-3,-16] # [-12,-4,-8,-17,-3]
+     rotate!(l2r_infnet_no3cyc,i)
+end
+
+rootonedge!(l2r_infnet,20);
+for i in [-17,-18,-19,-3,-6,-8,-16]
+     rotate!(l2r_infnet,i)
+end
+#= # now plot them
+R"pdf(file='../figures/netrax/multistart-set1b-L2R-15genes-no3cyc.pdf',width=8,height=8)"
 plot(l2r_infnet_no3cyc,majorhybridedgecolor="red",minorhybridedgecolor="blue",
      shownodelabel=true,edgewidth=2)
 R"dev.off()"
 
-## Multistart (15 genetrees), set1b, L2R, 15 genes, best network, 3-cycles not removed
 R"pdf(file='../figures/netrax/multistart-set1b-L2R-15genes-with3cyc.pdf',width=8,height=8)"
-rootonedge!(l2r_infnet,20);
-rotate!(l2r_infnet,-17);
-rotate!(l2r_infnet,-18);
-rotate!(l2r_infnet,-19);
-rotate!(l2r_infnet,-3);
-rotate!(l2r_infnet,-6);
-rotate!(l2r_infnet,-8);
-rotate!(l2r_infnet,-16);
 plot(l2r_infnet,majorhybridedgecolor="red",minorhybridedgecolor="blue",
      shownodelabel=true,edgewidth=2)
+R"dev.off()"
+=#
+
+# new plots, 2025-05
+l2r_infnet_no3cyc.node[findfirst(x -> x.name == "H0", l2r_infnet_no3cyc.node)].name = "H01"
+R"pdf"(file="../figures/netrax_15genes_8tax_l2r.pdf", width=12, height=6);
+R"layout"([1 2]);
+R"par"(mar=[0,0,0,0]);
+plot(l2r_infnet, majorhybridedgecolor="red", minorhybridedgecolor="blue",
+     shownodelabel=true, edgewidth=2, showgamma=true);
+plot(l2r_infnet_no3cyc, majorhybridedgecolor="red", minorhybridedgecolor="blue",
+     shownodelabel=true, edgewidth=2, showgamma=true);
 R"dev.off()"
