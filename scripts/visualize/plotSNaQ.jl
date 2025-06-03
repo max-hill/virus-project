f5bbe9d38b80c5cf180ae7cb5fe1598dca7d3959
Cecile Ane <cecileane@users.noreply.github.com>
2025-06-03 11:32:57 -0500

    fig: SNaQ nets with h=2

--- scripts/visualize/plotSNaQ.jl
+++ scripts/visualize/plotSNaQ.jl
@@ -41,6 +41,22 @@ R"ggsave"("../../figures/snaq_scores.pdf", width=5, height=3.5, units="in");
 # SNaQ networks with h=2
 
 using PhyloNetworks, PhyloPlots
+function breakedge!(net::HybridNetwork, lab::String)
+    it = findfirst(x -> x.name==lab, net.node)
+    isnothing(it) && error("taxon $lab not found in network")
+    e = getparentedge(net.node[it])
+    PhyloNetworks.breakedge!(e, net)
+end
+function renameTitatium!(net::HybridNetwork)
+    it = findfirst(x -> startswith(x.name, "Titanium"), net.node)
+    if isnothing(it)
+        @error("Titanium not found in network")
+        return nothing
+    end
+    net.node[it].name = "Ti"
+    return net
+end
+
 outdir_root = "../../analysis/snaq"
 n_8tax_SNP_file = joinpath(outdir_root,"fromSNPs", "set1b", "snaq_net_0123.out");
 n_9tax_SNP_file = joinpath(outdir_root,"fromSNPs", "set1c", "snaq_net_0123.out");
@@ -54,12 +70,20 @@ n_9tax_SNP = readmultinewick(n_9tax_SNP_file)[3]
 n_8tax_15gL2R = readmultinewick(n_8tax_15gL2R_file)[3]
 n_9tax_15gL2R = readmultinewick(n_9tax_15gL2R_file)[3]
 n_9tax_97gL2R = readmultinewick(n_9tax_97gL2R_file)[3]
-n_9tax_97gR2L = readmultinewick(n_9tax_97gR2L_file)[3]
+n_9tax_97gR2L_h1, n_9tax_97gR2L = readmultinewick(n_9tax_97gR2L_file)[[2,3]]
+# both incorrect: cannot be rooted with BHV5
+rootonedge!(n_9tax_97gR2L, 19) # to make BHV1.1 closer to being a clade
 
 nets = [n_8tax_SNP, n_9tax_SNP, n_8tax_15gL2R, n_9tax_15gL2R,
     n_9tax_97gL2R, n_9tax_97gR2L]
+renameTitatium!.(nets)
+
+cfinput = ["SNPs","SNPs","15 gene trees","15 gene trees",
+           "97 gene trees","97 gene trees"]
+details = ["- K22", "+ K22", "- K22", "+ K22", "(L2R)", "(R2L)"]
+
 # root with BHV5, if possible
-rootonedge!(nets[1], 3)
+rootonedge!(nets[1], 7)
 rootonedge!(nets[2], 11)
 rootonedge!(nets[3], 9)
 rootatnode!(nets[4], "BHV5")
@@ -67,6 +91,25 @@ rootonedge!(nets[5], 8)
 # reverse ismajor above BHV5: to show ML tree has a displayed tree
 nets[5].edge[ 8].ismajor = true
 nets[5].edge[11].ismajor = false
-rootonedge!(nets[6], 19) # to make BHV1.1 closer to being a clade
 
 # rotate edges to untangle them when plotting
+for i in [-9,-3,-10] rotate!(nets[1],i); end
+breakedge!(nets[1], "B589") # add degree-2 node: to move parent node to the left
+for i in [-11,-4,-3,-2,-8] rotate!(nets[2],i); end
+breakedge!(nets[2], "K22")
+for i in [-8,-9, -4,-3,-2] rotate!(nets[3],i); end
+for i in [-8,-9,-10, -4] rotate!(nets[4],i); end
+for i in [-5,-3, -11] rotate!(nets[5],i); end
+for i in [-5,-3,-4, -7,-8,-9] rotate!(nets[6],i); end
+
+xmax = [10,10,10,13.5,10.1,13.5]
+R"pdf"(file="../../figures/snaq_networks_h2.pdf", width=9, height=4.5);
+R"layout"(reshape(1:6, 2,3));
+R"par"(mar=[0,0,0,0]);
+for (i,n) in enumerate(nets)
+    plot(n; showgamma=true, xlim=[0.4,xmax[i]])
+    R"mtext"(cfinput[i], side=3, line=-1, adj=0, cex=0.8)
+    isempty(details[i]) ||
+    R"mtext"(details[i], side=3, line=-2.3, adj=0, cex=0.8)
+end
+R"dev.off"();
