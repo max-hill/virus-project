e7a44b27afcbbd068051b79b82ac4532f6283ed2
Max Hill <max6@hawaii.edu>
2026-04-03 20:58:34 -1000

    removed old plots, added experiment 24 plot

--- analysis/trilonet/plots-for-experiments-21-through-24.jl
+++ analysis/trilonet/plots-for-experiments-21-through-24.jl
@@ -17,7 +17,7 @@ using RCall
 #________________________________________________________________________________
 
 # The following function extracts networks from the output files of Trilonet
-function get_networks(taxonset, dir)
+function get_networks(taxonset, dir, experiment_number)
     pattern = "finalNetwork eNewick Short:"
     println("\n\n# Networks for ", taxonset, " from directory ", dir)
     results = []  # will store (kvalue, ksnippet, network)
@@ -26,7 +26,7 @@ function get_networks(taxonset, dir)
             m = match(r"-k([0-9]+(?:\.[0-9]+)?)\-", file)
             m === nothing && continue
             kvalue = parse(Float64, m.captures[1])
-            ksnippet = "net_k" * replace(m.captures[1], "." => "_")
+            ksnippet = "ex" * string(experiment_number) * "_net_k" * replace(m.captures[1], "." => "_")
             path = joinpath(dir, file)
             open(path) do io
                 lines = readlines(io)
@@ -50,8 +50,8 @@ end
 # EXPERIMENT 21 NETWORKS (major breakpoint only)
 
 # we run the following, which gives the code for the networks listed below
-get_networks("set1b","experiment-21-with-major-breakpoint-only")
-get_networks("set1c","experiment-21-with-major-breakpoint-only")
+get_networks("set1b","experiment-21-with-major-breakpoint-only",21)
+get_networks("set1c","experiment-21-with-major-breakpoint-only",21)
 
 # Ex21_Networks for set1b from directory experiment-21-with-major-breakpoint-only
 ex21_net_k0_5_set1b = readTopology("(BHV5,((B589,SP1777),(216_II,((Cooper,((C46,Titanium))#H1),(C33,#H1)))))root;")
@@ -165,8 +165,8 @@ R"dev.off"();
 # EXPERIMENT 22 NETWORKS (all breakpoints)
 
 # we run the following, which gives the code for the networks listed below
-get_networks("set1b","experiment-22-with-all-breakpoints")
-get_networks("set1c","experiment-22-with-all-breakpoints")
+get_networks("set1b","experiment-22-with-all-breakpoints",22)
+get_networks("set1c","experiment-22-with-all-breakpoints",22)
 
 # Networks for set1b from directory experiment-22-with-all-breakpoints
 ex22_net_k0_5_set1b = readTopology("(BHV5,((B589,SP1777),(216_II,(Cooper,(C33,(C46,Titanium))))))root;")
@@ -251,8 +251,8 @@ R"dev.off"();
 # EXPERIMENT 23 NETWORKS (no breakpoints)
 
 # we run the following, which gives the code for the networks listed below
-get_networks("set1b","experiment-23-with-no-breakpoints")
-get_networks("set1c","experiment-23-with-no-breakpoints")
+get_networks("set1b","experiment-23-with-no-breakpoints",23)
+get_networks("set1c","experiment-23-with-no-breakpoints",23)
 
 
 # Ex23_Networks for set1b from directory experiment-23-with-no-breakpoints
@@ -327,6 +327,88 @@ for (i,net) in enumerate(ex23_nets_set1b)
     R"mtext"("κ=$k", side=3, line=-1.5, adj=0.1, cex=0.8)
 end
 for (i,net) in enumerate(ex23_nets_set1c)
+    k = kappa_values[i]
+    plot(net, xlim=[xmin_set1c-xpad,xmax_set1c[i]+xpad], ylim=[ymin_set1c,ymax_set1c[i]],
+         showedgenumber=false, shownodenumber=false, minorlinetype="solid", arrowlen=0,
+         majorhybridedgecolor="deepskyblue", minorhybridedgecolor="deepskyblue", tipcex=.8)# tipcex=1.3
+    R"mtext"("κ=$k", side=3, line=-1.5, adj=0.1, cex=0.8)
+end
+R"dev.off"();
+
+
+
+
+# EXPERIMENT 24 NETWORKS (uniform 1500bp breakpoint mesh)
+
+# we run the following, which gives the code for the networks listed below
+get_networks("set1b","experiment-24-with-uniform-1500-bp-mesh",24)
+get_networks("set1c","experiment-24-with-uniform-1500-bp-mesh",24)
+
+# Networks for set1b from directory experiment-24-with-uniform-1500-bp-mesh
+ex24_net_k0_5_set1b = readTopology("(BHV5,((B589,SP1777),(216_II,(Cooper,(C33,(C46,Titanium))))))root;")
+ex24_net_k1_set1b = readTopology("(BHV5,((B589,SP1777),(216_II,(Cooper,(C33,(C46,Titanium))))))root;")
+ex24_net_k2_set1b = readTopology("(BHV5,((B589,SP1777),(216_II,(Cooper,(C33,(C46,Titanium))))))root;")
+ex24_net_k4_set1b = readTopology("(BHV5,((B589,SP1777),(216_II,((Cooper,(C33,(C46)#H1)),(Titanium,#H1)))))root;")
+ex24_net_k5_set1b = readTopology("(BHV5,((B589,SP1777),(216_II,((Cooper,(C46,(Titanium)#H1)),(C33,#H1)))))root;")
+ex24_net_k6_5_set1b = readTopology("(BHV5,(((C33,(Titanium,(C46,(Cooper)#H1))),(216_II,#H1)),(B589,SP1777)))root;")
+ex24_net_k8_set1b = readTopology("(BHV5,(((Cooper,(Titanium,(C33,(216_II)#H1))),(B589,SP1777)),(C46,#H1)))root;")
+ex24_net_k10_set1b = readTopology("(BHV5,(((Cooper,(Titanium,(C33,(216_II)#H1))),(B589,SP1777)),(C46,#H1)))root;")
+ex24_net_k20_set1b = readTopology("(BHV5,((SP1777,(B589,(216_II,(C33,(Titanium,(C46,#H1)))))),(Cooper)#H1))root;")
+
+# Networks for set1c from directory experiment-24-with-uniform-1500-bp-mesh
+ex24_net_k0_5_set1c = readTopology("(BHV5,(((B589,(K22)#H1),(SP1777,#H1)),(216_II,(Cooper,(C33,(C46,Titanium))))))root;")
+ex24_net_k1_set1c = readTopology("(BHV5,(((B589,(K22)#H1),(SP1777,#H1)),(216_II,(Cooper,(C33,(C46,Titanium))))))root;")
+ex24_net_k2_set1c = readTopology("(BHV5,(((B589,(K22)#H1),(SP1777,#H1)),(216_II,(Cooper,(C33,(C46,Titanium))))))root;")
+ex24_net_k4_set1c = readTopology("(BHV5,(((B589,(K22)#H1),(SP1777,#H1)),(216_II,((Cooper,(C33,(C46)#H2)),(Titanium,#H2)))))root;")
+ex24_net_k5_set1c = readTopology("(BHV5,(((B589,(K22)#H1),(SP1777,#H1)),(216_II,((Cooper,(C46,(Titanium)#H2)),(C33,#H2)))))root;")
+ex24_net_k6_5_set1c = readTopology("(BHV5,(((Cooper,(Titanium,(C33,(216_II)#H2))),(C46,#H2)),((B589,(K22)#H1),(SP1777,#H1))))root;")
+ex24_net_k8_set1c = readTopology("(BHV5,(((Cooper,(Titanium,(C33,(216_II)#H2))),(C46,#H2)),((SP1777,(K22,#H1)),(B589)#H1)))root;")
+ex24_net_k10_set1c = readTopology("(BHV5,(((Cooper,(Titanium,(C33,(216_II)#H1))),((SP1777,(K22,#H2)),(B589)#H2)),(C46,#H1)))root;")
+ex24_net_k20_set1c = readTopology("(BHV5,((K22,(B589,(216_II,(SP1777,(C33,(Titanium,(C46,#H1))))))),(Cooper)#H1))root;")
+
+# Save lists of the networks
+ex24_nets_set1b = [ex24_net_k0_5_set1b, ex24_net_k1_set1b, ex24_net_k2_set1b, ex24_net_k4_set1b, ex24_net_k5_set1b, ex24_net_k6_5_set1b, ex24_net_k8_set1b, ex24_net_k10_set1b, ex24_net_k20_set1b]
+ex24_nets_set1c = [ex24_net_k0_5_set1c, ex24_net_k1_set1c, ex24_net_k2_set1c, ex24_net_k4_set1c, ex24_net_k5_set1c, ex24_net_k6_5_set1c, ex24_net_k8_set1c, ex24_net_k10_set1c, ex24_net_k20_set1c]
+
+
+# TODO: untangle networks
+for i in [4,5,7,8] rotate!(ex24_nets_set1b[i],-10) end;
+for i in [7,8] rotate!(ex24_nets_set1b[i],-4) end;
+rotate!(ex24_nets_set1b[6],-9) end;
+for i in [1,2,3,4,5] rotate!(ex24_nets_set1c[i],-7) end;
+for i in [4,5,6] rotate!(ex24_nets_set1c[i],-13) end;
+for i in [6,7] rotate!(ex24_nets_set1c[i],-9) end;
+rotate!(ex24_nets_set1c[8],-4)
+rotate!(ex24_nets_set1c[8],-13)
+
+# Make dimensions for the plotting
+xmin_set1b = 0.7
+ymin_set1b = 0.9
+xmax_set1b = [12,12,12,12,13,13,13,13,13] #[11,11,11.4,12.3,11.3,13,13,12.4,13.4]
+ymax_set1b = [11,11,11,11,11,11,11,11,11] #[9.5,9.5,10.5,10.5,10.5,11.5,11.5,10.5,10.8]
+xmin_set1c = 0.7
+ymin_set1c = 0.9
+xmax_set1c = [12,12,12,12,13,13,13,15.5,15.5] #[9,9,10.4,10.3,10.3,12,11.2,12,14.4]
+ymax_set1c = [11,11,11,12,12,12,12,12,12] # [9.5,9.5,10.5,10.5,10.5,10.5,10.5,10.5,10.8]
+xpad = 0
+
+# Plot the networks in two 9x9 panels
+kappa_values = ["0.5","1","2","4","5","6.5","8","10","20"]
+R"cairo_pdf"("trilonet-with-uniform-1500-bp-mesh.pdf", height=5, width=12);
+R"layout"([1 2 3 0 10 11 12;
+           4 5 6 0 13 14 15;
+           7 8 9 0 16 17 18],
+    widths=[2 2 2 1 2 2 2]);
+R"par"(mar=[0,.5,0,.5]);
+for (i,net) in enumerate(ex24_nets_set1b)
+    k = kappa_values[i]
+    plot(net, xlim=[xmin_set1b-xpad,xmax_set1b[i]+xpad],
+         ylim=[ymin_set1b,ymax_set1b[i]],
+         showedgenumber=false, shownodenumber=false, minorlinetype="solid", arrowlen=0,
+         majorhybridedgecolor="deepskyblue", minorhybridedgecolor="deepskyblue", tipcex=.8) # tipcex=1.3
+    R"mtext"("κ=$k", side=3, line=-1.5, adj=0.1, cex=0.8)
+end
+for (i,net) in enumerate(ex24_nets_set1c)
     k = kappa_values[i]
     plot(net, xlim=[xmin_set1c-xpad,xmax_set1c[i]+xpad], ylim=[ymin_set1c,ymax_set1c[i]],
          showedgenumber=false, shownodenumber=false, minorlinetype="solid", arrowlen=0,
